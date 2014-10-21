f7376edb872557f62f22cd2edfe1be1b7d15c264
GAFuller <gareth.a.f@gmail.com>
2014-10-21 21:54:06 +0100

    Logo routes to main_app index

--- app/assets/javascripts/blogr/form.js
+++ app/assets/javascripts/blogr/form.js
@@ -3,6 +3,8 @@ $( document ).ready(function() {
     this.select();
   });
 
+  $('.editable').html($('.editable').data('content'));
+
   $('.editable').on('input', function() {
     $('#post_text').val($(this).html());
   });
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
   mount Blogr::Engine => "/blog"
+  root 'static#index'
 end
